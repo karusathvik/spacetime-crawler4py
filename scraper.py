9080d5364d2291c7df0148c54176ee48057c0d8d
Sathvik <karuturs@uci.edu>
2024-10-24 13:05:18 -0700

    partial

--- scraper.py
+++ scraper.py
@@ -1,21 +1,33 @@
 import re
+import requests
 from urllib.parse import urlparse
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+
+frequencies = dict() #for words
+subdomain_counts = dict() #subdomain like 'ics.uci.edu'
+visited_urls = set() #keeps tracks of visited and to avoid fragments
+polite_dict = dict() #key = domain, values = (allow_list,disallow_list)
+
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
     return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
-    # Implementation required.
-    # url: the URL that was used to get the page
-    # resp.url: the actual url of the page
-    # resp.status: the status code returned by the server. 200 is OK, you got the page. Other numbers mean that there was some kind of problem.
-    # resp.error: when status is not 200, you can check the error here, if needed.
-    # resp.raw_response: this is where the page actually is. More specifically, the raw_response has two parts:
-    #         resp.raw_response.url: the url, again
-    #         resp.raw_response.content: the content of the page!
-    # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    return list()
+	''' do i make this a generator function''' 
+	list_urls = []
+	if resp.status !=  200:
+		return list()
+	else:
+		frontier = {}
+
+		while 
+		 
+	
+
+
 
 def is_valid(url):
     # Decide whether to crawl this url or not. 
